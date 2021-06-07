28de747cdb6450263c3ab1b55c44029ade130000
Mario Hero <mariocanfly@126.com>
2021-06-07 14:31:05 +0800

    Update PicCompress.py

--- PicCompress.py
+++ PicCompress.py
@@ -8,13 +8,17 @@ import sys
 import os
 from PIL import Image
 
-SIZE_CUT = 4   # picture over this size should be compressed. Units: MB
+SIZE_CUT = 6   # picture over this size should be compressed. Units: MB
 QUALITY = 90  # 90 is good, this number should not be smaller than 80.
+
+
+
+SIZE_CUT_B = SIZE_CUT * 1024 * 1024
 
 
 def isPic(name):
     namelower = name.lower()
-    return namelower.endswith("jpeg") or namelower.endswith("jpg") or namelower.endswith("png")
+    return namelower.endswith("jpg") or namelower.endswith("jpeg") or namelower.endswith("png")
 
 
 def compressImg(file):
@@ -29,24 +33,25 @@ def compress(folder):
             print(folder)
             file_list = os.listdir(folder)
             for file in file_list:
-                if os.path.isdir(folder+"/"+file):
-                    #print(folder +"/"+ file)
-                    compress(folder +"/"+file)
+                temp = os.path.join(folder, file)
+                if os.path.isdir(temp):
+                    # print(temp)
+                    compress(temp)
                 else:
                     if isPic(file):
-                        if os.path.getsize(folder + "/" + file) > (SIZE_CUT * 1024 * 1024):
-                            compressImg(folder + "/" + file)
+                        if os.path.getsize(temp) > SIZE_CUT_B:
+                            compressImg(temp)
                             print(file)
         else:
             if isPic(folder):
-                if os.path.getsize(folder) > (SIZE_CUT * 1024 * 1024):
+                if os.path.getsize(folder) > SIZE_CUT_B:
                     compressImg(folder)
     except BaseException:
         return
 
 
 if __name__ == '__main__':
-    for folder in sys.argv:
+    for folder in sys.argv[1:]:
         #print(folder)
         compress(folder)
     print("Finish.")
